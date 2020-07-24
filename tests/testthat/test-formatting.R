86835b6abda1928aeede1dc09e32265b05c987ec
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-07-24 00:16:49 -0700

    Test bullet() without {cli}

--- tests/testthat/test-formatting.R
+++ tests/testthat/test-formatting.R
@@ -2,4 +2,5 @@ test_that("formatting without {crayon}", {
   local_mock(requireNamespace = function(...) {FALSE})
   expect_equal(code("code"), "`code`")
   expect_equal(cross_bullet(), paste0("\n", "x"))
+  expect_equal(bullet(), paste0("\n", "*"))
 })
