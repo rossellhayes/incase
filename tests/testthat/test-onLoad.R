d0e931e15472af742c806323fffbd8d1972b09da
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-03-27 14:02:41 -0700

    Remove onLoad from tests

--- tests/testthat/test-onLoad.R
+++
@@ -1,9 +0,0 @@
-test_that("lengths is backported", {
-  expect_silent(.onLoad(pkgname = "incase"))
-
-  if (getRversion() >= numeric_version("3.2.0")) {
-    expect_equal(environment(lengths), asNamespace("base"))
-  } else {
-    expect_equal(environment(lengths), asNamespace("backports"))
-  }
-})
